93fa458b6af6a863dd77a714260050effe9c4c1b
Rabba Alam <alam.nits@gmail.com>
2026-01-18 08:12:58 +0530

    adds quick hands on space for experimentation

---
+++ projects/quick-hands-on-space/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from quick-hands-on-space!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ projects/quick-hands-on-space/notebooks/utils.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+def get_today_str() -> str:
+    """Get current date in a human-readable format."""
+    return datetime.now().strftime("%a %b %-d, %Y")
